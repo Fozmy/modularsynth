ae7dbe4cd9a4699371915ffc3abe6a74e3f49d35
Andromeda Sawtelle <andromeda.sawtelle@gmail.com>
2022-02-08 10:19:23 -0500

    fixed oscillator freq

--- src/main/java/com/modularsynth/Oscillator.java
+++ src/main/java/com/modularsynth/Oscillator.java
@@ -2,6 +2,7 @@ package com.modularsynth;
 
 import net.beadsproject.beads.core.UGen;
 import net.beadsproject.beads.data.Buffer;
+import net.beadsproject.beads.ugens.Static;
 import net.beadsproject.beads.ugens.WavePlayer;
 
 public class Oscillator extends Module{
@@ -10,7 +11,7 @@ public class Oscillator extends Module{
 
     public Oscillator(audioOut ac) {
         super("Oscillator");
-        wp = new WavePlayer(ac.getAudioContext(), 440.0f, Buffer.SINE);
+        wp = new WavePlayer(ac.getAudioContext(), new Static(ac.getAudioContext(), 440.0f), Buffer.SINE);
     }
 
     @Override
